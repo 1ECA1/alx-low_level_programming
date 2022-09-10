38e581a7e00e08d683bce5f63063eca1e0626ad8
Ezenweani Alexander <alexanderalexone4@gmail.com>
2022-09-10 15:48:40 -0700

    HELLOPUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
+
+
 /*
+ *
  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *  * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  *
  */
 
-int main(void)  /* multilingual puzzle */
+int main(void)
 
 {
 
-			puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-						return (0);
+			return (0);
 
 }
