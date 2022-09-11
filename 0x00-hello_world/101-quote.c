397603d6a072f807845f98e2ad8a70b2055c2d6c
Ezenweani Alexander <alexanderalexone4@gmail.com>
2022-09-10 17:00:05 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
 
@@ -8,13 +9,12 @@
  *
  * Return: Always 1 (success)
  *
- /
+ */
 
-
-
-int main(void);
+int main(void)
 
 {
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 
