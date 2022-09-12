26fd48ac1c95d3f14ada3f43bfa7ad842ec86a31
Ezenweani Alexander <alexanderalexone4@gmail.com>
2022-09-12 08:09:42 -0700

    positive or negetive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,14 @@ srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf('%d is positive", n);
+		printf("%d is positive", n);
 	}
-	eles if (n==0)
+	else if (n==0)
 	{
-		printf ("%d is zero\n"'n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
 		printf("%d is negative\n", n);
 	}
-	return (0);
+return (0);
