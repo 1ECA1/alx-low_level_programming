5d41375698dacd7a9dec3fb638844c81243d2a9b
Ezenweani Alexander <alexanderalexone4@gmail.com>
2022-09-14 15:18:30 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,12 @@
  * Return: 0
  *
  */
-
-void print_alphabet(void)
+void print_alphabet _ finction(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	{
+char c;
+for (c = 'a'; c < = 'z'; c++)
+{
 _putchar(c);
-	}
-	_putchar('\n');
+}
+_putchar('\n');
 }
