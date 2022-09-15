ed9f7a2f4d6a8deb85c6ef2ed3adc31bfc3dc5f0
Ezenweani Alexander <alexanderalexone4@gmail.com>
2022-09-14 17:04:14 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,10 @@
 
 /**
  * print_alphabet_x10.c - print 10 times the alphabet, in lowercas
+ * Return: 0
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int count = 0;
 char letter;
